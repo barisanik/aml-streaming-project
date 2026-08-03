2fa41980632f7db576fff7b07f680734393cc4a3
Baris Anik <barisanik@gmail.com>
2026-08-03 21:55:50 +0300

    Modify schema add new tests for fields of stg_transactions and dbt_utils package
    Also add a custom test named test_contains_string which controls specified string in a column.

---
+++ dbt/aml_platform/macros/test_contains_string.sql
@@ -0,0 +1,29 @@
+/*
+	# ============================================================================ #
+		Macro: contains_string
+	# ============================================================================ #
+        Purpose: Detects if input value does not contain given character.
+        Logic: Returns values without required character. dbt marks a test as failed if this query returns any rows.
+
+        Usage on schema.yml:
+
+            models:
+            - name: model_name
+                columns:
+                - name: column_name
+                    tests:
+                    - contains_string:
+                        arguments:
+                            text: '-'
+
+	# ============================================================================ #
+*/
+{% test contains_string(model, column_name, text) %}
+
+SELECT {{ column_name }}
+FROM {{ model }}
+WHERE
+    {{ column_name }} IS NOT NULL
+    AND STRPOS({{ column_name }}, '{{ text }}') = 0
+
+{% endtest %}
